af0e616ef329d925b11bfe2bea36901feba99fd1
Pieter Nickmans <pieter.nickmans@phpro.be>
2019-12-26 21:27:06 +0100

    Fix dots and commas :)

--- src/app/modules/recipe/utils/shared-recipes.util.ts
+++ src/app/modules/recipe/utils/shared-recipes.util.ts
@@ -1,20 +1,20 @@
-import {SharedRecipe} from "../interfaces/recipe/shared-recipe.interface";
-import {RawSharedRecipe} from "../interfaces/api/raw-shared-recipe.interface";
-import {Recipe} from "../interfaces/recipe/recipe.interface";
-import {SharedUser} from "../interfaces/user/shared-user.interface";
+import {SharedRecipe} from '../interfaces/recipe/shared-recipe.interface';
+import {RawSharedRecipe} from '../interfaces/api/raw-shared-recipe.interface';
+import {Recipe} from '../interfaces/recipe/recipe.interface';
+import {SharedUser} from '../interfaces/user/shared-user.interface';
 
 export class SharedRecipesUtil {
     static createShareObject(recipe: Recipe, sharedUser: SharedUser): SharedRecipe {
         return {
             recipe: recipe,
             sharedUser: sharedUser
-        }
+        };
     }
 
     static sharedRecipeAsJSON(sharedRecipe: SharedRecipe): RawSharedRecipe {
         return {
             recipeId: sharedRecipe.recipe.id,
             userId: sharedRecipe.sharedUser.id
-        }
+        };
     }
 }
